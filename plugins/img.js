359d098ff7ef588052a821b0a0bf69a36226b76e
KermRayan <kermd237@gmail.com>
2024-11-28 23:20:44 +0100

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -43,7 +43,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
  *Enjoy these images! 👾*
 
-> 🌈*SILENT-SOBX-MD BOT PLUGINS*🏝️`
+> 🌈*KERM_MD-V4 BOT PLUGINS*🏝️`
 }, { quoted: mek });
 }
 
