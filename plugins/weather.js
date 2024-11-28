411d57281d193cc4ac2cfa55d66e1fc27397290c
KermRayan <kermd237@gmail.com>
2024-11-29 00:27:52 +0100

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 💨 *Wind Speed*: ${data.wind.speed} m/s
 🔽 *Pressure*: ${data.main.pressure} hPa
 
-*POWERED BY SILENTLOVER432*
+*POWERED BY KG TECH*
 `;
         return reply(weather);
     } catch (e) {
