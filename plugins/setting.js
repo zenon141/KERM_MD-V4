95b87915ed776548696abc7011ae7109e0174d8f
KermRayan <kermd237@gmail.com>
2024-11-29 00:06:32 +0100

    Update setting.js

--- plugins/setting.js
+++ plugins/setting.js
@@ -42,7 +42,7 @@ let madeSetting =`*╭────⚃* *SETTINGS* *⚃*
 *┊* *♾️ OWNER_REACT:* ➠ ${config.OWNER_REACT} 
 *┊* *♾️ BOT_NAME:* ➠ ${config.BOT_NAME}
 
-> © ᴄʀᴇᴀᴛᴇᴅ ʙʏ sɪʟᴇɴᴛʟᴏᴠᴇʀ⁴³²
+> © ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋɢ ᴛᴇᴄʜ 🇨🇲
 *╰═════════════⚃*
 `
 
