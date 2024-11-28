b470278bc3dda6ec84cb4afd4ac5e14e8e9176e3
KermRayan <kermd237@gmail.com>
2024-11-29 00:23:52 +0100

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -13,7 +13,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `*╭──────────●●►*
-*SILENT-SOBX-MD UPTIME LIST↷*
+*KERM_MD-V3 UPTIME LIST↷*
 
 *_UPTIME:➠_*  ${runtime(process.uptime())}
 
@@ -21,7 +21,7 @@ let status = `*╭──────────●●►*
 
 *_HOSTNAME:➠_* ${os.hostname()}
 
-*_OWNER:➠_* *SILENT LOVER⁴³²*
+*_OWNER:➠_* *KG TECH*
 *╰──────────●●►*
 `
 await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:`${status}`},{quoted:mek})
