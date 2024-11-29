c55bd2cfc86bcf15c9b8e6faf29d34d14d7b150b
KermRayan <kermd237@gmail.com>
2024-11-29 01:20:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_REACT: process.env.AUTO_REACT || "false",
-HEART_REACT: process.env.HEART_REACT || "true",
-OWNER_REACT: process.env.OWNER_REACT || "true",
+HEART_REACT: process.env.HEART_REACT || "false",
+OWNER_REACT: process.env.OWNER_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "➺『𝗞𝗘𝗥𝗠 𝗠𝗗 𝗩4』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
